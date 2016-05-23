46002f79886036f549cadfc3ceef509d2ea6b8fd
Cathy Oun <cathy810218@gmail.com>
2016-05-23 12:55:14 +0800

    Update AddDigits.java

--- Easy/AddDigits.java
+++ Easy/AddDigits.java
@@ -1,1 +1,27 @@
+public class Solution {
+    public int addDigits(int num) {
+        if (num < 10) {
+            return num;
+        }
+        int resultSum = 0;
+        while (num != 0) {
+            resultSum += num % 10;
+            num = num / 10;
+        }
+        return addDigits(resultSum);
+    }
+}
+
+
+// ----------------------------------------------------
+
+
+public class Solution {
+    public int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return num % 9 == 0 ? 9 : num % 9;
+    }
+}
 
