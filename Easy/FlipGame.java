b9a3f102211cf0d1b9629ba9db0d0d6f2f6a4087
Cathy Oun <cathy810218@gmail.com>
2016-05-23 10:27:51 +0800

    Update FlipGame.java

--- Easy/FlipGame.java
+++ Easy/FlipGame.java
@@ -1,1 +1,12 @@
-
+public class Solution {
+    public List<String> generatePossibleNextMoves(String s) {
+        List<String> result = new ArrayList<String>();
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '+' && s.charAt(i-1) == '+') {
+                String str = s.substring(0,i-1) + "--" + s.substring(i+1, s.length());
+                result.add(str);
+            }
+        }
+        return result;
+    }
+}
