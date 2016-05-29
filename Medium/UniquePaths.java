c277cf5ba8cde5193924fc76cba9ca4314f9c026
Cathy Oun <cathy810218@gmail.com>
2016-05-29 17:33:51 +0800

    Create UniquePaths.java

---
+++ Medium/UniquePaths.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        // create a cache matrix to store the possible paths
+        int[][] cache = new int[m][n];
+        for (int i = 0; i < m; i++ ) {
+            for (int j = 0; j < n; j++ ) {
+                if (i == 0 || j == 0) {
+                    cache[i][j] = 1; // if it at the edge, there's only one path
+                } else {
+                    cache[i][j] = cache[i][j-1] + cache[i-1][j];
+                }
+            }
+        }
+        return cache[m-1][n-1]; // the bottom right corner
+    }
+}
