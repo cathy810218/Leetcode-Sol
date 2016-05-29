b83abe4d86da9acf7e961171d300734a94725b22
Cathy Oun <cathy810218@gmail.com>
2016-05-29 19:06:24 +0800

    Update UniquePaths.java

--- Medium/UniquePaths.java
+++ Medium/UniquePaths.java
@@ -5,7 +5,7 @@ public class Solution {
         for (int i = 0; i < m; i++ ) {
             for (int j = 0; j < n; j++ ) {
                 if (i == 0 || j == 0) {
-                    cache[i][j] = 1; // if it at the edge, there's only one path
+                    cache[i][j] = 1; // if its at the edge, there's only one path
                 } else {
                     cache[i][j] = cache[i][j-1] + cache[i-1][j];
                 }
